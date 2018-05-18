488290031fcb4dee0663c73e4730925b206538c1
takahashi-tbn <kotaro_takahashi@tbn.co.jp>
2018-05-18 18:49:56 +0900

    first

---
+++ initializr/js/config.js
@@ -0,0 +1,9 @@
+
+var configConstants = {
+    auth0: {
+        domain: 'technobrain.auth0.com',
+        clientId: 'Nh4ugXqsgInIMtIToZQy2iS0xVpqB_t8',
+        clientSecret: 'VieK8ZQaax2Hv4eSR6gfxNRW4_L0PFdL5sPV7SzG8AmsPunVEnouKrjd7f'
+    },
+    apiBaseUrl: 'https://API-GATEWAY-URL/dev'
+};
